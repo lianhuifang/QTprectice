8f43c71a6aaa336ed08e2e5c407a22aa6afe17c2
lianhuifang <hfliann@163.com>
2025-01-30 14:55:15 +0800

    painter1 class 13 finish

--- painter_1/mainwindow.cpp
+++ painter_1/mainwindow.cpp
@@ -50,4 +50,21 @@ void MainWindow::paintEvent(QPaintEvent *)
     painter.setPen(QColor(Qt::red));
     //我们这里先让字体水平居中
     painter.drawText(rect, Qt::AlignHCenter, "yafeilinux");
+
+    QFont font("宋体", 15, QFont::Bold, true);
+    //设置下划线
+    font.setUnderline(true);
+    //设置上划线
+    font.setOverline(true);
+    //设置字母大小写
+    font.setCapitalization(QFont::SmallCaps);
+    //设置字符间的间距
+    font.setLetterSpacing(QFont::AbsoluteSpacing, 10);
+    //使用字体
+    painter.setFont(font);
+    painter.setPen(Qt::green);
+    painter.drawText(120, 80, tr("yafeilinux"));
+    painter.translate(50, 50);
+    painter.rotate(90);
+    painter.drawText(0, 0, tr("helloqt"));
 }
